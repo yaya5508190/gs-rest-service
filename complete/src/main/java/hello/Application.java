7cb3bab2536c7807811082ceb678f08397a09e47
yaya5508190 <357375084@qq.com>
2013-09-24 01:20:17 +0800

    去除@EnableAutoConfiguration注解
    理解spring-boot的初始化过程

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,13 +1,47 @@
 package hello;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
 
 @ComponentScan
-@EnableAutoConfiguration
 public class Application {
 
+   @Bean
+	public AnnotationMethodHandlerAdapter defultAbbitationAdapter(){
+		AnnotationMethodHandlerAdapter amhd = new AnnotationMethodHandlerAdapter();
+		HttpMessageConverter[] hmc = new HttpMessageConverter[1];
+		MappingJackson2HttpMessageConverter mj = new MappingJackson2HttpMessageConverter();
+		List<MediaType> l = new ArrayList<MediaType>();
+		l.add(MediaType.APPLICATION_JSON);
+		mj.setSupportedMediaTypes(l);
+		hmc[0]= new MappingJackson2HttpMessageConverter();
+		amhd.setMessageConverters(hmc);
+		return amhd;
+	}
+	@Bean
+	public EmbeddedServletContainerFactory defultEmbeddedServletContainerFactory(){
+		TomcatEmbeddedServletContainerFactory escf = new TomcatEmbeddedServletContainerFactory();
+		return escf;
+	}
+	
+	@Bean
+	public DispatcherServlet defultDispatcherServlet(){
+		DispatcherServlet dahm = new DispatcherServlet();
+		return dahm;
+		
+	}
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
